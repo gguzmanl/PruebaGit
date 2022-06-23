25c5575cd346e01157323248777a8c74e0957758
SISTEMAS FMS <gilopez428@gmail.com>
2022-06-23 17:02:38 -0500

    tercer commit

--- app/src/main/java/com/example/pruebagit/MainActivity.kt
+++ app/src/main/java/com/example/pruebagit/MainActivity.kt
@@ -1,37 +1,49 @@
 package com.example.pruebagit
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.pruebagit.databinding.ActivityMainBinding
+import  kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var usersDBHelper : UsersDBHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        usersDBHelper = UsersDBHelper(this)
+    }
+    fun addUser(v:View){
+        var userid = this.edittext_userid.text.toString()
+        var name = this.edittext_name.text.toString()
+        var age = this.edittext_age.text.toString()
+        var result = usersDBHelper.insertUser(UserModel(userid = userid,name = name,age = age))
+        //clear all edittext s
+        this.edittext_age.setText("")
+        this.edittext_name.setText("")
+        this.edittext_userid.setText("")
+        this.textview_result.text = "Added user : "+result
+        this.ll_entries.removeAllViews()
+    }
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    fun deleteUser(v:View){
+        var userid = this.edittext_userid.text.toString()
+        val result = usersDBHelper.deleteUser(userid)
+        this.textview_result.text = "Deleted user : "+result
+        this.ll_entries.removeAllViews()
+    }
 
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-        //prueba para edicion y subida
+    fun showAllUsers(v: View){
+        var users = usersDBHelper.readAllUsers()
+        this.ll_entries.removeAllViews()
+        users.forEach {
+            var tv_user = TextView(this)
+            tv_user.textSize = 30F
+            tv_user.text = it.name.toString() + " - " + it.age.toString()
+            this.ll_entries.addView(tv_user)
+        }
+        this.textview_result.text = "Fetched " + users.size + " users"
     }
 }
---
+++ app/src/main/java/com/example/pruebagit/UserModel.kt
@@ -0,0 +1,6 @@
+package com.example.pruebagit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserModel (val userid: String, val name: String, val age: String)
